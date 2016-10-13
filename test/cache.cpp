124805a67e28ba044af8346b61d70bc59c01994c
Alexander Batischev <eual.jp@gmail.com>
2016-10-13 20:45:54 +0300

    Test that cache::clean_old_articles does work

--- test/cache.cpp
+++ test/cache.cpp
@@ -42,3 +42,37 @@ TEST_CASE("cache behaves correctly") {
 
 	::unlink("test-cache.db");
 }
+
+TEST_CASE("Cleaning old articles works") {
+	/* Populating the cache file with some items. */
+	configcontainer * cfg = new configcontainer();
+	cache * rsscache = nullptr;
+	REQUIRE_NOTHROW(rsscache = new cache("test-cache.db", cfg));
+	rss_parser * parser = new rss_parser(
+			"file://data/rss.xml", rsscache, cfg, NULL);
+	std::shared_ptr<rss_feed> feed = parser->parse();
+	REQUIRE(feed->items().size() == 8);
+
+	/* Setting "keep-articles-days" to non-zero value to trigger
+	 * cache::clean_old_articles().
+	 *
+	 * The value of 42 days is sufficient because the items in the test feed
+	 * are dating back to 2006. */
+	cfg->set_configvalue("keep-old-articles", "42");
+
+	/* Simulating a restart of Newsbeuter. */
+	delete parser;
+	delete rsscache;
+	REQUIRE_NOTHROW(rsscache = new cache("test-cache.db", cfg));
+	rss_ignores * ign = new rss_ignores();
+	feed = rsscache->internalize_rssfeed("file://data/rss.xml", ign);
+
+	/* The important part: old articles should be gone. */
+	REQUIRE(feed->items().size() == 0);
+
+	/* Cleanup. */
+	delete ign;
+	delete rsscache;
+	delete cfg;
+	::unlink("test-cache.db");
+}
